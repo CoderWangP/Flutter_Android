ec57af182866c86480cba25c1c0f6abf9f59a62a
王鹏 <wp674512015@163.com>
2021-09-26 19:56:33 +0800

    清理无效代码

--- app/src/main/java/com/example/flutterandroiddemo/MainActivity.kt
+++ app/src/main/java/com/example/flutterandroiddemo/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.flutterandroiddemo
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -7,16 +6,9 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
-        private const val CHANNEL = "com.example.flutterandroiddemo/channel"
-    }
-
-//    private var mFlutterEngine :FlutterEngine? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        registerFlutterChannel()
     }
 
     fun forward2Flutter(view: View) {
@@ -28,17 +20,4 @@ class MainActivity : AppCompatActivity() {
             .initialRoute("/page_2")
             .build(this))
     }
-
-
-   /* private fun registerFlutterChannel() {
-        mFlutterEngine = FlutterEngine(this)
-        GeneratedPluginRegistrant.registerWith(mFlutterEngine!!)
-        MethodChannel(mFlutterEngine?.dartExecutor, CHANNEL)
-            .setMethodCallHandler { call, result ->
-                val method = call.method
-                if("withParams" == method){
-                    startActivity(Intent(this@MainActivity,NativeActivity::class.java))
-                }
-            }
-    }*/
 }
